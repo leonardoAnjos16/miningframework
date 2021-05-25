b72ceee54bfcc82c5bfae3d9a39ea5c6a434a701
Leonardo Anjos <las4@cin.ufpe.br>
2021-05-25 20:13:06 -0300

    Different since and until dates for each study regression test

--- src/test/regression/Study.groovy
+++ src/test/regression/Study.groovy
@@ -1,23 +1,41 @@
 package regression
 
-import injectors.S3MMiningModule
+import injectors.*
 
 enum Study {
 
-    DYNAMIC_ANALYSIS('dynamic-analysis.csv', S3MMiningModule),
-    S3M('S3M.csv', S3MMiningModule),
-    STATIC_ANALYSIS('static-analysis.csv', S3MMiningModule)
+    DYNAMIC_ANALYSIS('dynamic-analysis', '01/12/2020', '31/12/2020', StaticAnalysisConflictsDetectionModule),
+    S3M('S3M', '01/12/2020', '31/12/2020', S3MMiningModule),
+    STATIC_ANALYSIS('static-analysis', '01/12/2020', '31/12/2020', StaticAnalysisConflictsDetectionModule)
 
     private final String inputPath
+    private final String outputPath
+    private final String sinceDate
+    private final String untilDate
     private final Class injectorClass
 
-    Study(String inputName, Class injectorClass) {
-        this.inputPath = "src/test/regression/input/${inputName}"
+    private Study(String name, String sinceDate, String untilDate, Class injectorClass) {
+        this.inputPath = "src/test/regression/input/${name}.csv"
+        this.outputPath = "src/test/regression/expected/${name}/"
+        this.sinceDate = sinceDate
+        this.untilDate = untilDate
         this.injectorClass = injectorClass
     }
 
     String getInputPath() {
         return this.inputPath
+    }
+
+    String getOutputPath() {
+        return this.outputPath
+    }
+
+    String getSinceDate() {
+        return this.sinceDate
+    }
+
+    String getUntilDate() {
+        return this.untilDate
     }
 
     Class getInjectorClass() {
