0a2f407cec059d2470c82355bf15e81069e3ed0c
Leonardo Anjos <las4@cin.ufpe.br>
2021-05-25 20:14:48 -0300

    Refactoring and means to compare all the outputs of regression tests

--- src/test/regression/TestSuite.groovy
+++ src/test/regression/TestSuite.groovy
@@ -1,32 +1,39 @@
 package regression
 
-import static com.xlson.groovycsv.CsvParser.parseCsv
-
 import com.google.inject.*
 
 import org.junit.Assert
 import org.junit.Test
+
+import java.nio.file.Path
+import java.nio.file.Paths
 
 import app.MiningFramework
 import arguments.*
 import project.*
 
 class TestSuite {
-    private static final OUTPUT_PATH = 'src/test/regression/output'
+    private static final String OUTPUT_PATH = 'src/test/regression/output'
 
     @Test
     void DynamicAnalysisTest() {
-        runFramework(Study.DYNAMIC_ANALYSIS)
+        runTest(Study.DYNAMIC_ANALYSIS)
     }
 
     @Test
     void S3MTest() {
-        runFramework(Study.S3M)
+        runTest(Study.S3M)
     }
 
     @Test
     void StaticAnalysisTest() {
-        runFramework(Study.STATIC_ANALYSIS)
+        runTest(Study.STATIC_ANALYSIS)
+    }
+
+    private static void runTest(Study study) {
+        runFramework(study)
+        checkOutput(study)
+        deleteTestOutput()
     }
 
     private static void runFramework(Study study) {
@@ -34,7 +41,8 @@ class TestSuite {
 
         arguments.setInputPath(study.getInputPath())
         arguments.setOutputPath(OUTPUT_PATH)
-        arguments.setUntilDate('31/12/2020')
+        arguments.setSinceDate(study.getSinceDate())
+        arguments.setUntilDate(study.getUntilDate())
 
         Injector injector = Guice.createInjector(study.getInjectorClass().newInstance())
         MiningFramework framework = injector.getInstance(MiningFramework.class)
@@ -43,5 +51,47 @@ class TestSuite {
         ArrayList<Project> projects = InputParser.getProjectList(study.getInputPath())
         framework.setProjectList(projects)
         framework.start()
+    }
+
+    private static void checkOutput(Study study) {
+        Path actualOutput = Paths.get(OUTPUT_PATH)
+        Path expectedOutput = Paths.get(study.getOutputPath())
+        assert actualMatchesExpected(actualOutput, expectedOutput)
+    }
+
+    private static boolean actualMatchesExpected(Path actualOutput, Path expectedOutput) {
+        if (!actualOutput.exists() || expectedOutput.exists()) return false
+        if (actualOutput.getName() != expectedOutput.getName()) return false
+
+        if (actualOutput.isFile() && expectedOutput.isFile()) {
+            String actualContent = actualOutput.getText()
+            String expectedContent = expectedOutput.getText()
+            return actualContent == expectedContent
+        }
+
+        if (actualOutput.isDirectory && expectedOutput.isDirectory()) {
+            File[] actualFiles = actualOutput.listFiles()
+            File[] expectedFiles = expectedOutput.listFiles()
+
+            if (actualFiles.length != expectedFiles.length)
+                return false
+
+            Arrays.sort(actualFiles)
+            Arrays.sort(expectedFiles)
+
+            for (int i = 0; i < actualFiles.length; i++) {
+                if (!actualMatchesExpected(actualFiles, expectedFiles))
+                    return false
+            }
+
+            return true
+        }
+
+        return false
+    }
+
+    private static void deleteTestOutput() {
+        File outputDirectory = new File(OUTPUT_PATH)
+        assert outputDirectory.deleteDir()
     }
 }
